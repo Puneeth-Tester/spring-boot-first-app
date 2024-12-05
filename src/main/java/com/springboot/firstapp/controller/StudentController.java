368183531e84092c365986bf1feacd2cd24352fa
Puneeth-Tester <emailtopuneeth@gmail.com>
2024-12-05 09:20:17 +0530

    Spring Boot REST API - POST Request

--- src/main/java/com/springboot/firstapp/controller/StudentController.java
+++ src/main/java/com/springboot/firstapp/controller/StudentController.java
@@ -1,10 +1,8 @@
 package com.springboot.firstapp.controller;
 
 import com.springboot.firstapp.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,6 +63,18 @@ public class StudentController {
             @RequestParam String firstName,
             @RequestParam String lastName){
         return new Student(id, firstName, lastName);
+    }
+
+    // Spring boot Rest API that handles HTTP Post Request
+    // @PostMapping and @RequestBody
+    // http://localhost:8084/students/create
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }
 
 }
